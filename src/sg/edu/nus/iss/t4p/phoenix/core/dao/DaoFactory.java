269321378f62037ce17c835c43375f8c5d539345
windieccf <windieccf@gmail.com>
2012-09-20 14:08:23 +0000

    add javadoc

--- src/sg/edu/nus/iss/t4p/phoenix/core/dao/DaoFactory.java
+++ src/sg/edu/nus/iss/t4p/phoenix/core/dao/DaoFactory.java
@@ -1,3 +1,20 @@
+/*
+ * CONFIDENTIAL AND PROPRIETARY SOURCE CODE OF
+ * Institute of Systems Science, National University of Singapore
+ *
+ * Copyright 2012 Team 4(Part-Time), ISS, NUS, Singapore. All rights reserved.
+ * Use of this source code is subjected to the terms of the applicable license
+ * agreement.
+ *
+ * -----------------------------------------------------------------
+ * REVISION HISTORY
+ * -----------------------------------------------------------------
+ * DATE             AUTHOR          REVISION		DESCRIPTION
+ * 15 Sep 2012    	Team 4		    0.1				Initial creating
+ * 													
+ * 													
+ * 
+ */
 package sg.edu.nus.iss.t4p.phoenix.core.dao;
 
 import sg.edu.nus.iss.t4p.phoenix.dao.programslot.ProgramSlotDao;
--- src/sg/edu/nus/iss/t4p/phoenix/core/exceptions/BusinessLogicException.java
+++ src/sg/edu/nus/iss/t4p/phoenix/core/exceptions/BusinessLogicException.java
@@ -1,8 +1,32 @@
+/*
+ * CONFIDENTIAL AND PROPRIETARY SOURCE CODE OF
+ * Institute of Systems Science, National University of Singapore
+ *
+ * Copyright 2012 Team 4(Part-Time), ISS, NUS, Singapore. All rights reserved.
+ * Use of this source code is subjected to the terms of the applicable license
+ * agreement.
+ *
+ * -----------------------------------------------------------------
+ * REVISION HISTORY
+ * -----------------------------------------------------------------
+ * DATE             AUTHOR          REVISION		DESCRIPTION
+ * 15 Sep 2012    	Team 4		    0.1				Initial creating
+ * 													
+ * 													
+ * 
+ */
 package sg.edu.nus.iss.t4p.phoenix.core.exceptions;
 
+/**
+ * Business logic exception. This exception will be thrown if
+ * the updating is against the business rule.
+ **/
 @SuppressWarnings("serial")
 public class BusinessLogicException extends Exception{
-	
+	/**
+	 * Constructor for BusinessLogicException. The input message is returned in
+	 * toString() message.
+	 */
 	public BusinessLogicException(String msg) {
 		super(msg);
 	}
