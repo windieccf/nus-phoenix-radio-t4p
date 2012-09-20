0f7532487617e5ae14d1d4a97223e7cff934d904
mansoor.mi08 <mansoor.mi08@gmail.com>
2012-09-20 14:59:02 +0000

    amended for javadoc

--- src/sg/edu/nus/iss/t4p/phoenix/action/security/AuthenticateController.java
+++ src/sg/edu/nus/iss/t4p/phoenix/action/security/AuthenticateController.java
@@ -1,3 +1,20 @@
+/*
+ * CONFIDENTIAL AND PROPRIETARY SOURCE CODE OF
+ * Institute of Systems Science, National University of Singapore
+ *
+ * Copyright 2012 Team 4(Part-Time), ISS, NUS, Singapore. All rights reserved.
+ * Use of this source code is subjected to the terms of the applicable license
+ * agreement.
+ *
+ * -----------------------------------------------------------------
+ * REVISION HISTORY
+ * -----------------------------------------------------------------
+ * DATE             AUTHOR          REVISION		DESCRIPTION
+ * 20 Sep 2012    	Team 4		    1.0				Initial creating
+ * 													
+ * 													
+ * 
+ */
 package sg.edu.nus.iss.t4p.phoenix.action.security;
 
 import java.io.IOException;
@@ -14,10 +31,26 @@ import sg.edu.nus.iss.t4p.phoenix.delegate.security.AuthenticateDelegate;
 import sg.edu.nus.iss.t4p.phoenix.entity.user.User;
 import sg.edu.nus.iss.t4p.phoenix.utility.T4StringUtil;
 
+/**
+* Login Authentication Controller Action class 
+* @author Mansoor M I A0092661A
+* @version 1.0
+*/
 @SuppressWarnings("serial")
 @WebServlet(urlPatterns = "/authenticateController/*")
 public class AuthenticateController extends BaseController {
 	
+	/**
+	 * Method to authenticate users by sending in User class. 
+	 * Authenticated users will be redirected to the requested page.
+	 * Method will throw exception if either User Name or Password left empty.
+	 * @param request the HttpServletRequest
+	 * @param response the HttpServletResponse
+	 * @exception javax.servlet.ServletException
+	 * @exception java.io.IOException
+	 * @see javax.servlet.ServletException
+	 * @see java.io.IOException
+	 */
 	protected void doLogin(HttpServletRequest request,	HttpServletResponse response) throws ServletException, IOException{
 		User user = super.retrieveParameter(request,User.class);
 		try{
@@ -36,6 +69,15 @@ public class AuthenticateController extends BaseController {
 		}
 	}
 	
+	/**
+	 * Method to perform logout for authenticated users.
+	 * @param request the HttpServletRequest
+	 * @param response the HttpServletResponse
+	 * @exception javax.servlet.ServletException
+	 * @exception java.io.IOException
+	 * @see javax.servlet.ServletException
+	 * @see java.io.IOException
+	 */	
 	protected void doLogout(HttpServletRequest request,	HttpServletResponse response) throws ServletException, IOException{
 		request.getSession().invalidate();
 		request.getRequestDispatcher("/").forward(request, response);
