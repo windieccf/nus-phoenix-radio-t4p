7e5e4ea4b458dc24dd27d4621d603d99b42479bb
windieccf <windieccf@gmail.com>
2012-09-20 13:10:13 +0000

    for javadoc

--- src/sg/edu/nus/iss/t4p/phoenix/action/radioprogram/RadioProgramController.java
+++ src/sg/edu/nus/iss/t4p/phoenix/action/radioprogram/RadioProgramController.java
@@ -1,3 +1,20 @@
+/*
+ * CONFIDENTIAL AND PROPRIETARY SOURCE CODE OF
+ * Institute of Systems Science, National University of Singapore
+ *
+ * Copyright 2012 Team 4(Part-Time), ISS, NUS, Singapore. All rights reserved.
+ * Use of this source code is subjected to the terms of the applicable license
+ * agreement.
+ *
+ * -----------------------------------------------------------------
+ * REVISION HISTORY
+ * -----------------------------------------------------------------
+ * DATE             AUTHOR          REVISION		DESCRIPTION
+ * 15 Sep 2012    	Team 4		    0.1				Initial creating
+ * 													
+ * 													
+ * 
+ */
 package sg.edu.nus.iss.t4p.phoenix.action.radioprogram;
 
 import java.io.IOException;
@@ -14,11 +31,20 @@ import sg.edu.nus.iss.t4p.phoenix.delegate.radioprogram.RadioProgramDelegate;
 import sg.edu.nus.iss.t4p.phoenix.entity.radioprogram.RadioProgram;
 import sg.edu.nus.iss.t4p.phoenix.utility.T4StringUtil;
 
-
+/**
+ * Servlet implementation class for RadioProgramController
+ */
 @SuppressWarnings("serial")
 @WebServlet(urlPatterns = "/radioprogramController/*")
 public class RadioProgramController extends BaseController {
 	
+	/**
+	 * List radio programs
+	 * @param request
+	 * @param response
+	 * @throws ServletException
+	 * @throws IOException
+	 */
 	protected void doList(HttpServletRequest request,	HttpServletResponse response) throws ServletException, IOException{
 
 		List<RadioProgram> radioprograms = RadioProgramDelegate.getInstance().retrieveProgramList();
@@ -33,7 +59,13 @@ public class RadioProgramController extends BaseController {
 	}
 	
 	
-	
+	/**
+	 * Select radio program
+	 * @param request
+	 * @param response
+	 * @throws ServletException
+	 * @throws IOException
+	 */
 	protected void doSelectRadioProgram(HttpServletRequest request,	HttpServletResponse response)throws ServletException, IOException{
 		RadioProgram radioProgram = super.retrieveParameter(request, RadioProgram.class);
 		String callBackUrl = request.getParameter(ConstantAttribute.CALL_BACK_URL);
