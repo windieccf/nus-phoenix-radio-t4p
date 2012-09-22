c1fed4f4ad246d89707ed4669ff16d14f5864e0d
mansoor.mi08 <mansoor.mi08@gmail.com>
2012-09-22 00:22:07 +0000

    javadoc changes

--- src/sg/edu/nus/iss/t4p/phoenix/action/presenterproducer/PresenterProducerController.java
+++ src/sg/edu/nus/iss/t4p/phoenix/action/presenterproducer/PresenterProducerController.java
@@ -1,3 +1,20 @@
+/*
+ * CONFIDENTIAL AND PROPRIETARY SOURCE CODE OF
+ * Institute of Systems Science, National University of Singapore
+ *
+ * Copyright 2012 Team 4(Part-Time), ISS, NUS, Singapore. All rights reserved.
+ * Use of this source code is subjected to the terms of the applicable license
+ * agreement.
+ *
+ * -----------------------------------------------------------------
+ * REVISION HISTORY
+ * -----------------------------------------------------------------
+ * DATE             AUTHOR          REVISION		DESCRIPTION
+ * 20 Sep 2012    	Team 4		    1.0				Initial creating
+ * 													
+ * 													
+ * 
+ */
 package sg.edu.nus.iss.t4p.phoenix.action.presenterproducer;
 
 import java.io.IOException;
@@ -14,11 +31,24 @@ import sg.edu.nus.iss.t4p.phoenix.delegate.user.UserDelegate;
 import sg.edu.nus.iss.t4p.phoenix.entity.user.User;
 import sg.edu.nus.iss.t4p.phoenix.utility.T4StringUtil;
 
-
+/**
+* Presenter Producer Controller Action class 
+* @author Robin Foe A0092657U
+* @version 1.0
+*/
 @SuppressWarnings("serial")
 @WebServlet(urlPatterns = "/PresenterProducerController/*")
 public class PresenterProducerController extends BaseController {
 	
+	/**
+	 * Method to retrieve the list of presenters and forward the request to list_presenter page
+	 * @param request the HttpServletRequest
+	 * @param response the HttpServletResponse
+	 * @exception javax.servlet.ServletException
+	 * @exception java.io.IOException
+	 * @see javax.servlet.ServletException
+	 * @see java.io.IOException
+	 */
 	protected void doPresenterList(HttpServletRequest request,	HttpServletResponse response) throws ServletException, IOException{
 
 		List<User> presenters = UserDelegate.getInstance().retrievePresenterProducerList(true);
@@ -31,6 +61,15 @@ public class PresenterProducerController extends BaseController {
 		request.getRequestDispatcher("/pages/presenter/list_presenter.jsp").forward(request, response);		
 	}
 	
+	/**
+	 * Method to retrieve a particular presenter based on User class passed in as parameter in http request.
+	 * @param request the HttpServletRequest
+	 * @param response the HttpServletResponse
+	 * @exception javax.servlet.ServletException
+	 * @exception java.io.IOException
+	 * @see javax.servlet.ServletException
+	 * @see java.io.IOException
+	 */
 	protected void doSelectPresenter(HttpServletRequest request,	HttpServletResponse response)throws ServletException, IOException{
 		User presenter = super.retrieveParameter(request, User.class);
 		String callBackUrl = request.getParameter(ConstantAttribute.CALL_BACK_URL);
@@ -38,6 +77,15 @@ public class PresenterProducerController extends BaseController {
 		request.getRequestDispatcher(callBackUrl).forward(request, response);	
 	}
 	
+	/**
+	 * Method to retrieve the list of producers and forward the request to list_producer page
+	 * @param request the HttpServletRequest
+	 * @param response the HttpServletResponse
+	 * @exception javax.servlet.ServletException
+	 * @exception java.io.IOException
+	 * @see javax.servlet.ServletException
+	 * @see java.io.IOException
+	 */	
 	protected void doProducerList(HttpServletRequest request,	HttpServletResponse response) throws ServletException, IOException{
 
 		List<User> producers = UserDelegate.getInstance().retrievePresenterProducerList(false);
@@ -50,6 +98,15 @@ public class PresenterProducerController extends BaseController {
 		request.getRequestDispatcher("/pages/producer/list_producer.jsp").forward(request, response);		
 	}
 	
+	/**
+	 * Method to retrieve a particular producer based on User class passed in as parameter in http request.
+	 * @param request the HttpServletRequest
+	 * @param response the HttpServletResponse
+	 * @exception javax.servlet.ServletException
+	 * @exception java.io.IOException
+	 * @see javax.servlet.ServletException
+	 * @see java.io.IOException
+	 */	
 	protected void doSelectProducer(HttpServletRequest request,	HttpServletResponse response)throws ServletException, IOException{
 		User producer = super.retrieveParameter(request, User.class);
 		String callBackUrl = request.getParameter(ConstantAttribute.CALL_BACK_URL);
